677a6f5ebc049efbb5b93e8899208028e059f9b6
Karthik VR <karthikvr0312@gmail.com>
2026-08-16 12:04:40 +0530

    Kafka Fallback

--- src/main/java/net/engineeringdigest/journalApp/scheduler/UserScheduler.java
+++ src/main/java/net/engineeringdigest/journalApp/scheduler/UserScheduler.java
@@ -63,8 +63,11 @@ public class UserScheduler {
             }
             if (mostFrequesntSentiment != null) {
                 SentimentData sentimentData = SentimentData.builder().email(user.getEmail()).sentiment("Sentiment for last 7 days"+mostFrequesntSentiment).build();
-                kafkaTemplate.send("weekly-sentiments",sentimentData.getEmail(),sentimentData);
-            }
+                try{
+                    kafkaTemplate.send("weekly-sentiments", sentimentData.getEmail(), sentimentData);
+                }catch (Exception e){
+                    emailService.sendEmail(sentimentData.getEmail(), "Sentiment for previous week", sentimentData.getSentiment());
+                }            }
             log.info("Mail Sent");
         }
     }
